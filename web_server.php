44fb61434e5ad074bf1d85cf9d0b500206ce946c
Dheeraj Nair <Dheeraj1998@users.noreply.github.com>
2017-02-28 23:02:57 +0530

    Update web_server.php

--- web_server.php
+++ web_server.php
@@ -1,1 +1,17 @@
+<?php
 
+//Login details
+$servername = "mysql4.gear.host";
+$username = "bloodbank";
+$password = "Wk602y_N66F_";
+
+// Create connection
+$dbh = new PDO("mysql:host=$servername", $username, $password); 
+
+// Check connection
+if ($dbh->connect_error) {
+    die("Connection failed: " . $dbh->connect_error);
+}
+
+echo "Connected successfully";
+?>
