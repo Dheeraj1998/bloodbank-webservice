39e6379f17e8c793721311e846dd39f1d34e687a
Dheeraj Nair <Dheeraj1998@users.noreply.github.com>
2017-03-01 00:06:20 +0530

    Used mysqli instead of PDO
    This was changed for easier usage of the calls to the database to receive data.

--- web_server.php
+++ web_server.php
@@ -1,17 +1,49 @@
 <?php
-
 //Login details
 $servername = "mysql4.gear.host";
 $username = "bloodbank";
 $password = "Wk602y_N66F_";
+$dbname = "bloodbank";
 
-// Create connection
-$dbh = new PDO("mysql:host=$servername", $username, $password); 
+//Create connection
+$conn = new mysqli("$servername", $username, $password, $dbname);
 
-// Check connection
-if ($dbh->connect_error) {
-    die("Connection failed: " . $dbh->connect_error);
+//Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+//echo "Connected successfully";
+
+//Create table query
+//$sql = "CREATE TABLE BloodBank (UserName VARCHAR(40) NOT NULL, Password VARCHAR(40) NOT NULL, Name VARCHAR(40) NOT NULL , BloodType VARCHAR(3) NOT NULL , Location VARCHAR(40) NOT NULL, Allergies VARCHAR(40) NULL, PRIMARY KEY (UserName))";
+
+//if ($conn->query($sql) === TRUE) {
+//    echo "Table BloodBank created successfully!";} 
+//else {
+//    echo "Error creating table: " . $conn->error;
+//}
+
+//Inserting data
+//$sql = "INSERT INTO BloodBank (UserName, Password, Name, BloodType, Location, Allergies) VALUES ('Dheeraj1998', 'Dheeraj@1998', 'Dheeraj Nair', 'B+', 'Jamnagar', 'Cold, Cough')";
+
+//if ($conn->query($sql) === TRUE) {
+//    echo "\nNew record created successfully!";
+//} else {
+//    echo "\nError: " . $sql . "<br>" . $conn->error;
+//}
+
+//Selecting all data
+$sql = "SELECT * FROM BloodBank";
+$result = mysqli_query($conn ,$sql);
+	
+while ($row = mysqli_fetch_assoc($result)) {
+	$array[] = $row;
 }
 
-echo "Connected successfully";
+header('Content-Type:Application/json');
+echo json_encode($array);
+mysqli_free_result($result);
+
+$conn->close();
 ?>
