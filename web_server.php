af3f00ba407f4130ab6a48f8ccacde6638e951a4
Dheeraj Nair <nair.dheeraj@yahoo.co.in>
2017-05-01 10:45:10 +0530

    Added options for account deletion.

--- web_server.php
+++ web_server.php
@@ -53,7 +53,7 @@ elseif(isset($_GET['type'])){
     $location = $_GET['location'];
     $allergy = $_GET['allergies'];
     
-    $sql = "INSERT INTO BloodBank (UserName, Password, Name, BloodType, MobNumber, Location, Allergies) VALUES ($username, $password, $name, $blood, $mobnumber, $location, $allergy)";
+    $sql = "INSERT INTO BloodBank (Username, Password, Name, BloodType, MobNumber, Location, Allergies) VALUES ($username, $password, $name, $blood, $mobnumber, $location, $allergy)";
 
     if ($conn->query($sql) === TRUE) {
         echo "You have been registered successfully!";
@@ -65,7 +65,7 @@ elseif(isset($_GET['type'])){
 //Validating the user according to the Username
 //Sample URL call is : http://localhost/PHP_Tutorial/web_service.php?username="Dheeraj1998"&password="Dheeraj%401998"
 elseif(!isset($_GET['login']) && isset($_GET['username']) && isset($_GET['password'])){
-    $sql = "SELECT Password FROM BloodBank WHERE UserName = " . $_GET['username'];
+    $sql = "SELECT Password FROM BloodBank WHERE Username = " . $_GET['username'];
     
     $result = mysqli_query($conn ,$sql);
     $row = mysqli_fetch_assoc($result);
@@ -100,7 +100,7 @@ elseif(isset($_GET['bloodtype'])){
 //Getting the details of a user by validating his username and password
 //Sample URL call is: http://localhost/PHP_Tutorial/web_service.php?login=''&username='Dheeraj'&password='123'
 elseif(!isset($_GET['modify']) && isset($_GET['login']) && isset($_GET['username']) && isset($_GET['password'])){
-    $sql = "SELECT * FROM BloodBank WHERE UserName = " . $_GET['username'] . " AND Password = " . $_GET['password'];
+    $sql = "SELECT * FROM BloodBank WHERE Username = " . $_GET['username'] . " AND Password = " . $_GET['password'];
 
     $result = mysqli_query($conn ,$sql);
     $row = $result->fetch_assoc();
@@ -113,10 +113,20 @@ elseif(!isset($_GET['modify']) && isset($_GET['login']) && isset($_GET['username
 //Modifying the details of a user by validating his username and password
 //Sample URL call is: http://dheerajprojects.gear.host/web_server.php?modify=''&username='Dheeraj1998'&mpass='123'&maller='Cold'&mblood='A%2B'&mmobilenumber="1231231231"&mname="Dheeraj"&mloc="Jamnagar"
 elseif(isset($_GET['modify']) && isset($_GET['username'])){
-    $sql = "UPDATE BloodBank SET Password = " . $_GET['mpass'] . ", Name = " . $_GET['mname'] . ", Location = " . $_GET['mloc'] . ", Allergies = " . $_GET['maller'] . ", MobNumber = " . $_GET['mmobilenumber'] . ", BloodType = " . $_GET['mblood'] . " WHERE UserName = " . $_GET['username'];
+    $sql = "UPDATE BloodBank SET Password = " . $_GET['mpass'] . ", Name = " . $_GET['mname'] . ", Location = " . $_GET['mloc'] . ", Allergies = " . $_GET['maller'] . ", MobNumber = " . $_GET['mmobilenumber'] . ", BloodType = " . $_GET['mblood'] . " WHERE Username = " . $_GET['username'];
     
     if ($conn->query($sql) === TRUE) {
         echo "Details have been modified!";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+}
+
+elseif(isset($_GET['delete_account']) && isset($_GET['username'])){
+    $sql = "DELETE FROM BloodBank WHERE Username = " . $_GET['username'];
+    
+    if ($conn->query($sql) === TRUE) {
+        echo "Account has been deleted!";
     } else {
         echo "Error: " . $sql . "<br>" . $conn->error;
     }
